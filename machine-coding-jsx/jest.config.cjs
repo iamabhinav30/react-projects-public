7a1afd5251986d93b5b62c9d0e269e03c9f31a0f
Abhinav Singh <iamabhinav30@gmail.com>
2025-04-30 08:28:21 +0530

    Test cases for COntact Form

--- machine-coding-jsx/jest.config.cjs
+++ machine-coding-jsx/jest.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  transform: { '^.+\\.[jt]sx?$': 'babel-jest' },
-  moduleNameMapper: { '\\.(css|scss)$': 'identity-obj-proxy' },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleFileExtensions: ['js', 'jsx', 'json']
-};
+   roots: ['<rootDir>/src'],
+     testEnvironment: 'jest-environment-jsdom',
+     transform: {
+       '^.+\\.[jt]sx?$': 'babel-jest'
+     },
+     moduleNameMapper: {
+       '\\.(css|scss)$': 'identity-obj-proxy'
+     },
+     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+     moduleFileExtensions: ['js', 'jsx', 'json']
+  };
+  
--- machine-coding-jsx/src/__tests__/ContactForm.test.jsx
+++ machine-coding-jsx/src/__tests__/ContactForm.test.jsx
@@ -1,9 +1,35 @@
-const { render, screen } = require("@testing-library/react")
+import React from 'react';
+const { render, screen, fireEvent } = require("@testing-library/react");
 import ContactForm from '../Pages/ContactForm';
 
 describe('ContactForm details',()=>{
     it('renders all fields',()=>{
         render (<ContactForm />);
         expect(screen.getByLabelText(/Name:/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Email:/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Message:/i)).toBeInTheDocument();
+    }),
+    it('show validation message on Empty Submit', async ()=>{
+        render(<ContactForm />);
+        fireEvent.click(screen.getByRole('button',{name : /submit/i}));
+        expect(await screen.findByText(/Name is required/i)).toBeInTheDocument();
+        expect(await screen.findByText(/Email is required/i)).toBeInTheDocument();
+        expect(await screen.findByText(/Message is required/i)).toBeInTheDocument();
+    }),
+    it('Submit successfully and display Thank you', async()=>{
+        render(<ContactForm />);
+        fireEvent.change(screen.getByLabelText(/Name:/i),{target:{value: 'Alice'}});
+        fireEvent.change(screen.getByLabelText(/Email/i),{target :{value : 'abhi@xyz.com'}});
+        fireEvent.change(screen.getByLabelText(/Message:/i),{target: {value: 'Hello Here is the message'}});
+        fireEvent.click(screen.getByRole('button', {name: /submit/i}));
+        expect(await screen.findByText(/Thank you, Alice/)).toBeInTheDocument();
+    })
+    it('Email Id not corrent and clicked on Submit', async()=>{
+        render(<ContactForm />);
+        fireEvent.change(screen.getByLabelText(/Name:/i),{target:{value: 'Alice'}});
+        fireEvent.change(screen.getByLabelText(/Email/i),{target :{value : 'abhixyz.com'}});
+        fireEvent.change(screen.getByLabelText(/Message:/i),{target: {value: 'Hello Here is the message'}});
+        fireEvent.click(screen.getByRole('button', {name: /submit/i}));
+        expect(await screen.findByText(/Invalid Email format/)).toBeInTheDocument();
     })
 })
